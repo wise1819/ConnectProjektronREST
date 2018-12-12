9ec084a4f439f503dab2c395252bc1ab2253a936
manuel leppert <manuelleppert@freenet.de>
2018-12-12 11:07:20 +0100

    init

---
+++ src/main/java/AuthenticationObject.java
@@ -0,0 +1,59 @@
+import com.mashape.unirest.http.HttpMethod;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequest;
+import com.mashape.unirest.request.HttpRequestWithBody;
+
+
+/**
+ * A simple class that constructs base REST-Requests based on the data from the authentication step
+ */
+public class AuthenticationObject {
+    private String mobileAppTokenCookie;
+    private String xCsrfToken;
+
+    AuthenticationObject(String mobileAppTokenCookie, String xCsrfToken){
+        this.mobileAppTokenCookie = xCsrfToken;
+        this.xCsrfToken = mobileAppTokenCookie;
+    }
+
+    /**
+     *
+     * @param type Type of the request you want to make => get, post, put
+     * @return A Object that has the necessary authentication data already filled in
+     */
+    public HttpResponse<JsonNode> prepareRequest(HttpVerb type, String url){
+        try {
+        switch (type) {
+            case HttpVerbs:
+                return prepareGetRequest(url);
+            case "post":
+                return preparePostRequest(url);
+            case "put":
+                return preparePutRequest(url);
+            default:
+                 System.err.println("No legal http-verb chosen, use \"get, post or put\"");
+                 throw new IllegalArgumentException();
+        }}
+        catch (UnirestException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private HttpResponse<JsonNode> prepareGetRequest(String url) throws UnirestException {
+         return Unirest.get(url).header("Cookie", this.mobileAppTokenCookie).header("X-CSRF-Token", this.xCsrfToken)
+                .header("cache-control", "no-cache").asJson();
+    }
+
+    private HttpResponse<JsonNode> preparePostRequest(String url) throws UnirestException{
+        return  Unirest.post(url).header("Cookie", this.mobileAppTokenCookie).header("X-CSRF-Token", this.xCsrfToken)
+                .header("cache-control", "no-cache").asJson();
+    };
+    private HttpResponse<JsonNode> preparePutRequest(String url) throws  UnirestException{
+        return Unirest.put(url).header("Cookie", this.mobileAppTokenCookie).header("X-CSRF-Token", this.xCsrfToken)
+                .header("cache-control", "no-cache").asJson();
+    };
+}
--- src/main/java/UnirestAuthorization.java
+++ src/main/java/UnirestAuthorization.java
@@ -1,9 +1,5 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.mashape.unirest.http.HttpResponse;
@@ -15,48 +11,46 @@ public class UnirestAuthorization {
 
 	public static void main(String[] args) throws UnirestException {
 
-		String username = ""; // TODO: fill in blanks
-		String password = "";
+		String username = "Admin"; // TODO: fill in blanks
+		String password = "Admin";
+		String urlBCS = "http://fuberlinws18.demo.projektron.de";
+		String urlRESTPath = "/app/rest/timerecording/bookings?syncStateTimestamp=0&minDate=2018-10-29";
+
 		if (username.equals("") || password.equals("")) {
 			System.out.println("Please provide Username and password");
 			return;
 		}
-		List<JSONObject> bookings = getBookings(username, password);
 
-		bookings.forEach(System.out::println);
+		AuthenticationObject ao = authenticate(username, password, urlBCS);
+
+
+		HttpResponse<JsonNode> req = ao.prepareRequest(HttpVerb.GET, urlBCS + urlRESTPath );
+
+
+		//HttpResponse<JsonNode> res = Unirest.get(urlBCS + urlRESTPath);
+
+
 	}
 
-	private static List<JSONObject> getBookings(String username, String password) throws UnirestException {
-		List<JSONObject> bookings = new ArrayList<JSONObject>();
+	private static AuthenticationObject authenticate(String username, String password, String urlBCS) throws UnirestException{
 
 		HashMap<String, String> data = new HashMap<>();
 		data.put("userLogin", username);
 		data.put("userPwd", password);
 
-		HttpResponse<String> authResponse = Unirest.post("https://fu-projekt.bcs-hosting.de/app/rest/auth/login")
+		HttpResponse<String> authResponse = Unirest.post(urlBCS + "/app/rest/auth/login")
 				.header("Content-Type", "application/json").header("cache-control", "no-cache")
 				.header("Content-Encoding", "UTF-8").body(new JSONObject(data).toString()).asString();
 		String allCookies = authResponse.getHeaders().get("set-cookie");
 		int beginIndex = allCookies.indexOf("MobileAppToken");
 		int endIndex = allCookies.substring(beginIndex).indexOf(";");
-		String mobileAppTokenCookie = allCookies.substring(beginIndex, endIndex);
-		String xCsrfToken = authResponse.getHeaders().get("X-CSRF-Token");
-
-		HttpResponse<JsonNode> timeRecordingResponse = Unirest.get(
-				"https://fu-projekt.bcs-hosting.de/app/rest/timerecording/bookings?syncStateTimestamp=0&minDate=2018-10-29")
-				.header("Cookie", mobileAppTokenCookie).header("X-CSRF-Token", xCsrfToken)
-				.header("cache-control", "no-cache").asJson();
-
-		try {
-
-			JSONArray bookingsJsonArray = timeRecordingResponse.getBody().getObject().getJSONArray("bookings");
-			for (int i = 0; i < bookingsJsonArray.length(); i++) {
-				bookings.add(bookingsJsonArray.getJSONObject(i));
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return bookings;
+		return new AuthenticationObject(allCookies.substring(beginIndex, endIndex),authResponse.getHeaders().get("X-CSRF-Token"));
 	}
+
+
+public enum HttpVerb {
+		GET,POST,PUT
+}
+
 
 }
